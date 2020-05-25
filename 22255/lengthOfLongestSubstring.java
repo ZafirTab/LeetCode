95c62cc3e1925672852a1210ea4b3c7c6970667f
Zhitao Hu <huzhitao2916@126.com>
2020-05-25 17:54:05 +0800

    Update lengthOfLongestSubstring.java

--- 22255/lengthOfLongestSubstring.java
+++ 22255/lengthOfLongestSubstring.java
@@ -43,8 +43,3 @@ class Solution {
         return ans;
     }
 }
-
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
